8dd8ef4a6ec2d437f6bb266ad2beed3578c19265
itsEKO <91019944+itsEKO@users.noreply.github.com>
2025-09-02 23:24:01 -0500

    assignment 01
    upload edited code

--- apps/hanoi/hanoi.cpp
+++ apps/hanoi/hanoi.cpp
@@ -3,33 +3,37 @@
 using namespace std;
 
 void moveDisks(int, int, int, int);
-void printIt(int , int , int );
+void printIt(int, int, int);
 
 int main() {
     const int FROMPEG = 1;
     const int TOPEG = 3;
     const int TEMPPEG = 2;
-    const int NUMDISKS = 3;
-    for (auto numdisks: {3,5,6}) {
-      cout << "Numdisks: " << numdisks << endl;
-      moveDisks(numdisks, FROMPEG, TOPEG, TEMPPEG);
-      cout << "Moved " << numdisks << " pegs"
-           << " from peg " << FROMPEG
-           << " to peg " << TOPEG << endl;
+
+    cout << "--- Recursive Version Timings ---" << endl;
+
+    for (int numdisks : {5, 10, 15, 20, 25, 30, 35}) {
+        cout << "Number of disks: " << numdisks << endl;
+
+        auto start = chrono::high_resolution_clock::now();
+
+        moveDisks(numdisks, FROMPEG, TOPEG, TEMPPEG);
+
+        auto stop = chrono::high_resolution_clock::now();
+        auto duration = chrono::duration_cast<chrono::milliseconds>(stop - start);
+        cout << "Time taken: " << duration.count() << " milliseconds" << endl;
+        cout << "---------------------------------" << endl;
     }
+    return 0;
 }
 
 void moveDisks(int num, int fromPeg, int toPeg, int tempPeg) {
-     if (num > 0) {
-        moveDisks(num -1, fromPeg, tempPeg, toPeg);
-        printIt(num, fromPeg, toPeg);
-        moveDisks(num-1, tempPeg, toPeg, fromPeg);
-     }
+    if (num > 0) {
+        moveDisks(num - 1, fromPeg, tempPeg, toPeg);
+        moveDisks(num - 1, tempPeg, toPeg, fromPeg);
+    }
 }
 
 void printIt(int disk, int fromPeg, int toPeg) {
-  // Do nothing for timing test, but otherwise
-  cout<< "Move disk " << disk
-      << " from pef " << fromPeg
-      << " to peg " << toPeg << endl;
+    // keep empty
 }
--- apps/hanoi/hanoiit.cpp
+++ apps/hanoi/hanoiit.cpp
@@ -5,86 +5,87 @@
 using namespace std;
 
 void moveDisks(int, stack<int> &, stack<int> &, stack<int>&);
-void printIt(int , char , char );
-void MoveDisksHelper(stack<int> &, stack<int> &, char , char ) ;
+void printIt(int, char, char);
+void MoveDisksHelper(stack<int> &, stack<int> &, char, char);
 
 int main() {
-  for (auto numdisks: {3,5,6}) {
-      stack<int> source;
-      stack<int> dest;
-      stack<int> aux;
-      cout << "Numdisks: " << numdisks << endl;
-      moveDisks(numdisks, source, aux, dest);
-      cout << "Moved " << numdisks << " pegs." <<  endl;
+    cout << "--- Iterative Version Timings ---" << endl;
+
+    for (int numdisks : {5, 10, 15, 20, 25, 30, 35}) {
+        stack<int> source;
+        stack<int> dest;
+        stack<int> aux;
+        
+        cout << "Number of disks: " << numdisks << endl;
+
+        auto start = chrono::high_resolution_clock::now();
+
+        moveDisks(numdisks, source, aux, dest);
+
+        auto stop = chrono::high_resolution_clock::now();
+        auto duration = chrono::duration_cast<chrono::milliseconds>(stop - start);
+        cout << "Time taken: " << duration.count() << " milliseconds" << endl;
+        cout << "---------------------------------" << endl;
     }
     return 0;
 }
 
 void moveDisks(int num, stack<int> &fromPeg, stack<int> &tempPeg, stack<int> &toPeg) {
-  char s = 'S', d = 'D', a = 'A';
-  if (num % 2 == 0) { // Swap in num disks is even.
-    char temp = d;
-    d = a;
-    a = temp;
-  }
-  int numberOfMoves = pow(2, num) - 1;
-  for (int i = num; i >= 1; i--) {
-    fromPeg.push(i);
-  }
-  // now do moves:
-  for (int i = 1; i <= numberOfMoves; i++) {
-    if (i % 3 == 0) {
-      MoveDisksHelper(tempPeg, toPeg, a, d);
+    char s = 'S', d = 'D', a = 'A';
+    if (num % 2 == 0) {
+        char temp = d;
+        d = a;
+        a = temp;
     }
-    else if (i % 3 == 2) {
-      MoveDisksHelper(fromPeg,tempPeg, s, a);
+    long long numberOfMoves = pow(2, num) - 1; 
+    for (int i = num; i >= 1; i--) {
+        fromPeg.push(i);
     }
-    else if (i % 3 ==  1) {
-      MoveDisksHelper(fromPeg,toPeg, s, d);
+
+    for (long long i = 1; i <= numberOfMoves; i++) {
+        if (i % 3 == 0) {
+            MoveDisksHelper(tempPeg, toPeg, a, d);
+        }
+        else if (i % 3 == 2) {
+            MoveDisksHelper(fromPeg, tempPeg, s, a);
+        }
+        else if (i % 3 == 1) {
+            MoveDisksHelper(fromPeg, toPeg, s, d);
+        }
     }
-  }
 }
 
 void MoveDisksHelper(stack<int> &source, stack<int> &dest, char s, char d) {
-  int top1;
-  int top2;
-  if (source.empty()) {
-    top2 = dest.top();
-    dest.pop();
-    source.push(top2);
-    printIt(top2, d, s);
-  }
-  else if (dest.empty()) {
-    top1 = source.top();
-    source.pop();
-    dest.push(top1);
-    printIt(top1, s, d);
-  }
-  else if (source.top() > dest.top()) {
-    top1 = source.top();
-    source.pop();
-    top2 = dest.top();
-    dest.pop();
-    source.push(top1);
-    source.push(top2);
-    printIt(top2,d, s);
-  }
-  else {
-    top2 = dest.top();
-    dest.pop();
-    top1 = source.top();
-    source.pop();
-    dest.push(top2);
-    dest.push(top1);
-    printIt(top1, s, d);
-  }
+    int top1;
+    int top2;
+    if (source.empty()) {
+        top2 = dest.top();
+        dest.pop();
+        source.push(top2);
+    }
+    else if (dest.empty()) {
+        top1 = source.top();
+        source.pop();
+        dest.push(top1);
+    }
+    else if (source.top() > dest.top()) {
+        top1 = source.top();
+        source.pop();
+        top2 = dest.top();
+        dest.pop();
+        source.push(top1);
+        source.push(top2);
+    }
+    else {
+        top2 = dest.top();
+        dest.pop();
+        top1 = source.top();
+        source.pop();
+        dest.push(top2);
+        dest.push(top1);
+    }
 }
 
 void printIt(int disk, char fromPeg, char toPeg) {
-  // Do nothing for timing test, but otherwise
-  std::cout<< "Move disk "<<disk
-           << "from peg " << fromPeg
-           << "to peg " << toPeg << std::endl;
+    // keep empty
 }
-
-
